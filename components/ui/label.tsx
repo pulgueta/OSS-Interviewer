8ce9ad0701758b098fcb164677b46a21aaa78fd1
pulgueta <retardix456@gmail.com>
2024-04-23 20:40:44 -0500

    feat: add new shadcn components

---
+++ components/ui/label.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import {
+	type ComponentPropsWithoutRef,
+	type ElementRef,
+	forwardRef,
+} from 'react';
+
+import { Root } from '@radix-ui/react-label';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+
+const labelVariants = cva(
+	'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+);
+
+const Label = forwardRef<
+	ElementRef<typeof Root>,
+	ComponentPropsWithoutRef<typeof Root> & VariantProps<typeof labelVariants>
+>(({ className, ...props }, ref) => (
+	<Root ref={ref} className={cn(labelVariants(), className)} {...props} />
+));
+Label.displayName = Root.displayName;
+
+export { Label };
