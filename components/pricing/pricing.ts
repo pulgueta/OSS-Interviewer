a9b9e318e67882995a765be55c29c714281ed4c4
pulgueta <retardix456@gmail.com>
2024-04-24 00:28:30 -0500

    chore: add missing information to pricing

---
+++ components/pricing/pricing.ts
@@ -0,0 +1,72 @@
+export const pricingData = [
+	{
+		title: 'Basic',
+		type: 'personal',
+		price: 0,
+		description: 'Casual practice for your next job.',
+		features: [
+			'Basic AI.',
+			'3 Interviews per month.',
+			'Basic feedback review.',
+			'Data retention for 15 days.',
+		],
+	},
+	{
+		title: 'Professional',
+		type: 'personal',
+		price: 5.99,
+		description: 'Become a professional at job interviews.',
+		features: [
+			'Stronger AI.',
+			'10 Interviews per month.',
+			'Advanced feedback review.',
+			'Data retention for 60 days.',
+		],
+	},
+	{
+		title: 'Premium',
+		type: 'personal',
+		price: 9.99,
+		description: 'Never miss the chance to land your next dream job.',
+		features: [
+			'Strongest AI.',
+			'20 Interviews per month.',
+			'Advanced feedback review.',
+			'Data retention forever.',
+		],
+	},
+	{
+		title: 'Team',
+		type: 'enterprise',
+		price: 29.99,
+		description: 'For small teams requiring personnel.',
+		features: [
+			'Strongest AI.',
+			'60 Interviews per month',
+			'Advanced feedback and performance review on candidate.',
+		],
+	},
+	{
+		title: 'Enterprise',
+		type: 'enterprise',
+		price: 49.99,
+		description:
+			'Focus on your product instead of the full-time recruiting process',
+		features: [
+			'Strongest AI.',
+			'150 Interviews per month',
+			'Advanced feedback and performance review on candidate.',
+		],
+	},
+	{
+		title: 'Custom',
+		type: 'enterprise',
+		price: 99.99,
+		description: 'The best recruiting system for the best companies.',
+		features: [
+			'Strongest AI.',
+			'Contact sales for custom interviews amount',
+			'Advanced feedback and performance review on candidate.',
+		],
+	},
+];
