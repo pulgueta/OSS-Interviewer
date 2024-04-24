f95ee271885126e9003fd607b86fc54cf71fffa8
pulgueta <retardix456@gmail.com>
2024-04-23 23:20:28 -0500

    feat: add register page

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from '@/components/form/auth/register-form';
+
+const Register = () => {
+	return (
+		<div className='flex min-h-svh flex-col items-center justify-center p-4'>
+			<RegisterForm />
+		</div>
+	);
+};
+export default Register;
