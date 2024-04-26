c5ad40f9d29e8fcd79f8e981c8872ea69e0df46a
pulgueta <retardix456@gmail.com>
2024-04-26 04:07:14 -0500

    feat: add encrypt and decrypt functions with argon2 and a createId function with nanoid

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,27 @@
+import { hash, verify } from 'argon2';
+import { customAlphabet } from 'nanoid';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+import { env } from '@/env.server';
+
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const encrypt = async (value: string | Buffer) =>
+	await hash(value, { secret: env.ARGON_SECRET });
+
+export const decrypt = async (hash: string, value: string | Buffer) =>
+	await verify(hash, value, { secret: env.ARGON_SECRET });
+
+const nanoid = customAlphabet(
+	'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
+);
+
+export const prefixes = {
+	user: 'user',
+	teams: 'teams',
+	account: 'account',
+} as const;
+
+export const createId = (prefix: keyof typeof prefixes): string =>
+	[prefixes[prefix], nanoid(20)].join('_');
