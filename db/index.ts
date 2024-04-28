0fb7b11ea0792191f03a46dc38bc14cebc98450c
pulgueta <retardix456@gmail.com>
2024-04-28 17:20:44 -0500

    refactor: add db schemas to drizzle function

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,8 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
+import * as schema from '@/db/schema';
 import { env } from '@/env.server';
 
 const sql = neon(env.DATABASE_URL);
-export const db = drizzle(sql);
+export const db = drizzle(sql, { schema });
