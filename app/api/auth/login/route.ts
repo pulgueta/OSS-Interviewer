95058d4d5eb70fa0784b64246974981607c0c448
pulgueta <retardix456@gmail.com>
2024-05-02 12:33:51 -0500

    refactor: move try-catch block to separate function due to Next.js bug

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -8,12 +8,76 @@ import { signIn } from '@/auth';
 import { loginSchema } from '@/schemas/loginSchema';
 import { default as translations } from '@/i18n/en.json';
 
+const authenticate = async ({
+	email,
+	password,
+}: {
+	email: string;
+	password: string;
+}) => {
+	const { _401, _404, _500 } = translations.login.api;
+
+	try {
+		const user = await getUserByEmail(email);
+
+		if (!user) {
+			return Response.json({ message: _404 }, { status: 404 });
+		}
+
+		if (!user.emailVerified) {
+			return Response.json(
+				{ message: _401.noEmailVerified },
+				{ status: 401 },
+			);
+		}
+
+		try {
+			await signIn('credentials', {
+				email,
+				password,
+			});
+
+			return true;
+		} catch (error) {
+			if (error instanceof AuthError) {
+				switch (error.type) {
+					case 'CredentialsSignin':
+						return Response.json(
+							{
+								message: _401.invalidCredentials,
+							},
+							{ status: 401 },
+						);
+					case 'AccessDenied':
+						return Response.json(
+							{ message: _401.accessDenied },
+							{ status: 401 },
+						);
+					default:
+						return Response.json(
+							{ message: _500 },
+							{ status: 500 },
+						);
+				}
+			}
+
+			console.log(error);
+
+			throw error;
+		}
+	} catch (error) {
+		if (error instanceof Error) {
+			return Response.json({ message: _500, error }, { status: 500 });
+		}
+	}
+};
+
 export const POST = async (req: NextRequest) => {
 	const ip = req.ip ?? '127.0.0.1';
 
 	const allowed = await ratelimiter.limit(ip);
 
-	const { _200, _400, _401, _404, _500 } = translations.login.api;
+	const { _400, _200 } = translations.login.api;
 	const { api } = translations;
 
 	if (!allowed.success) {
@@ -41,57 +105,9 @@ export const POST = async (req: NextRequest) => {
 
 	const { email, password } = body.data;
 
-	try {
-		const user = await getUserByEmail(email);
+	const auth = await authenticate({ email, password });
 
-		if (!user) {
-			return Response.json({ message: _404 }, { status: 404 });
-		}
-
-		if (!user.emailVerified) {
-			return Response.json(
-				{ message: _401.noEmailVerified },
-				{ status: 401 },
-			);
-		}
-
-		try {
-			await signIn('credentials', {
-				email,
-				password,
-				redirectTo: '/dashboard',
-				redirect: true,
-			});
-		} catch (error) {
-			if (error instanceof AuthError) {
-				switch (error.type) {
-					case 'CredentialsSignin':
-						return Response.json(
-							{
-								message: _401.invalidCredentials,
-							},
-							{ status: 401 },
-						);
-					case 'AccessDenied':
-						return Response.json(
-							{ message: _401.accessDenied },
-							{ status: 401 },
-						);
-					default:
-						return Response.json(
-							{ message: _500 },
-							{ status: 500 },
-						);
-				}
-			}
-
-			throw error;
-		}
-
+	if (auth) {
 		return Response.json({ message: _200 }, { status: 200 });
-	} catch (error) {
-		if (error instanceof Error) {
-			return Response.json({ message: _500, error }, { status: 500 });
-		}
 	}
 };
