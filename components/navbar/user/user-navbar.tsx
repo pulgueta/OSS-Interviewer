e84daa701a97483eab6b5e228609690338571cb9
pulgueta <retardix456@gmail.com>
2024-05-04 19:11:57 -0500

    feat: add verification on user account type to redirect to one site or another

--- components/navbar/user/user-navbar.tsx
+++ components/navbar/user/user-navbar.tsx
@@ -6,15 +6,46 @@ import type { CustomUser } from '@/auth';
 import { ThemeSwitcher } from '../../theme-switcher';
 import { LogoutButton } from './logout-button';
 import { MobileMenu } from './mobile-menu';
+import { currentUser } from '@/lib/get-session';
+import { getUserById } from '@/lib/db/users';
+import { getAccountsById } from '@/lib/db/accounts';
 import { default as translations } from '@/i18n/en.json';
 
 export const UserNavbar: FC<CustomUser> = async ({}) => {
+	const user = await currentUser();
+
+	if (!user) return;
+
+	const { accounts } = await getUserById(user.id);
+
+	if (!accounts) return;
+
+	const data = accounts.reduce((acc, item) => {
+		if (acc.accountType === 'personal') {
+			const { id, accountType } = item;
+
+			// @ts-ignore
+			acc[accountType] = id;
+		}
+
+		return acc;
+	});
+
+	// @ts-ignore
+	const [account] = await getAccountsById(data.id);
+
+	if (!account) return;
+
 	const { title } = translations.dashboard.navbar;
 
 	return (
 		<header className='flex items-center justify-between border-b bg-background p-4 shadow md:justify-around'>
 			<Link
-				href='/dashboard'
+				href={
+					account.accountType === 'enterprise'
+						? '/dashboard/enterprise'
+						: '/dashboard'
+				}
 				className='text-2xl font-bold tracking-tight md:text-3xl'
 				prefetch={false}
 			>
