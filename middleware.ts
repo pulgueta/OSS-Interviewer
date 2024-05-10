e3ed1b9cbebac9f03e61726781f14a9a8e308c37
pulgueta <retardix456@gmail.com>
2024-05-09 20:33:41 -0500

    feat: add middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,4 @@
-import { auth } from '@/auth';
-
-export default auth((req) => {
-	const isLogged = !!req.auth;
-	const isDashboardRoute = req.nextUrl.pathname.startsWith('/dashboard');
-	const redirect = req.url.replace(req.nextUrl.pathname, '/login');
-
-	if (isDashboardRoute) {
-		if (isLogged) return true;
-
-		return false;
-	} else if (isLogged) {
-		return Response.redirect(redirect);
-	}
-
-	return true;
-});
+export { auth as middleware } from '@/auth';
 
 export const config = {
 	matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
