199a4b6dbbb34272d4a2cbbcb49afe6f7bbd8a2a
pulgueta <retardix456@gmail.com>
2024-05-09 22:33:31 -0500

    feat: modify return type

--- lib/db/main.config.ts
+++ lib/db/main.config.ts
@@ -9,7 +9,7 @@ export class DatabaseService {
 	async getAllData<const T>(table: AnyPgTable) {
 		const query = await db.select().from(table);
 
-		return query as T;
+		return Array.isArray(query) ? (query as T[]) : ([query] as T);
 	}
 
 	async getBy<const T>(
@@ -19,7 +19,7 @@ export class DatabaseService {
 	) {
 		const query = await db.select().from(table).where(eq(column, payload));
 
-		return query as T;
+		return Array.isArray(query) ? (query as T[]) : ([query] as T);
 	}
 
 	async insertToDB<const T>(
