3504995a1c8c1edea7493a8b562b9669e67f824c
pulgueta <retardix456@gmail.com>
2024-05-11 14:23:12 -0500

    refactor: return instantly the signOut function and redirect user

--- components/navbar/actions/logout.ts
+++ components/navbar/actions/logout.ts
@@ -1,11 +1,6 @@
 'use server';
 
-import { revalidatePath } from 'next/cache';
-
 import { signOut } from '@/auth';
 
-export const logout = async () => {
+export const logout = async () =>
 	await signOut({ redirect: true, redirectTo: '/login' });
-
-	return revalidatePath('/dashboard');
-};
