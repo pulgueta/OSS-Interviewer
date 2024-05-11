13395c1f164bfb972a00dd3bef3a0e92cbc597e1
pulgueta <retardix456@gmail.com>
2024-05-11 18:01:24 -0500

    feat: modify navbar layout

--- components/navbar/user/user-navbar.tsx
+++ components/navbar/user/user-navbar.tsx
@@ -1,72 +1,74 @@
-import type { FC } from 'react';
-
 import Link from 'next/link';
 
-import type { CustomUser } from '@/auth';
 import { ThemeSwitcher } from '../../theme-switcher';
 import { LogoutButton } from './logout-button';
 import { MobileMenu } from './mobile-menu';
 import { currentUser } from '@/lib/get-session';
-import { getUserById } from '@/lib/db/users';
+import { getUserByEmail } from '@/lib/db/users';
+import { buttonVariants } from '@/components/ui/button';
 import { getAccountsById } from '@/lib/db/accounts';
 import { default as translations } from '@/i18n/en.json';
 
-export const UserNavbar: FC<CustomUser> = async ({}) => {
+export const UserNavbar = async () => {
 	const user = await currentUser();
 
 	if (!user) return;
 
-	const { accounts } = await getUserById(user.id);
+	const { accounts } = await getUserByEmail(user.email);
 
 	if (!accounts) return;
 
-	const data = accounts.reduce((acc, item) => {
+	const data = accounts.reduce((acc: { [key: string]: string }, item) => {
 		if (acc.accountType === 'personal') {
 			const { id, accountType } = item;
 
-			// @ts-ignore
 			acc[accountType] = id;
 		}
 
 		return acc;
-	});
+	}, {});
 
-	// @ts-ignore
-	const [account] = await getAccountsById(data.id);
+	const account = await getAccountsById(data.id);
 
 	if (!account) return;
 
 	const { title } = translations.dashboard.navbar;
+	const { links } = translations.navbar.user;
+
+	const href =
+		account.accountType === 'enterprise'
+			? '/dashboard/enterprise'
+			: '/dashboard';
 
 	return (
 		<header className='flex items-center justify-between border-b bg-background p-4 shadow md:justify-around'>
 			<Link
-				href={
-					account.accountType === 'enterprise'
-						? '/dashboard/enterprise'
-						: '/dashboard'
-				}
-				className='text-2xl font-bold tracking-tight md:text-3xl'
+				href={href}
+				className='text-3xl font-extrabold tracking-tighter'
 				prefetch={false}
 			>
 				{title}
 			</Link>
 
 			<nav className='flex items-center gap-x-4 md:gap-x-8'>
-				<ul className='flex items-center gap-x-8 font-medium'>
-					{translations.navbar.user.links.map((link) => (
+				<ul className='flex items-center gap-x-4 font-medium'>
+					{links.map((link) => (
 						<li className='hidden md:flex' key={link.href}>
-							<Link href={link.href} prefetch={false}>
+							<Link
+								href={link.href}
+								prefetch={false}
+								className={buttonVariants({
+									variant: 'ghost',
+								})}
+							>
 								{link.label}
 							</Link>
 						</li>
 					))}
-				</ul>
-				<div className='flex items-center gap-x-2 md:gap-x-4'>
 					<ThemeSwitcher />
 					<LogoutButton />
 					<MobileMenu />
-				</div>
+				</ul>
 			</nav>
 		</header>
 	);
